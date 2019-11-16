0c60825cb785be0d16f6049d2f89926a328f8740
EthanBautista <raffybautista07@gmail.com>
2019-11-15 23:40:51 -0500

    Added some css for menu table

--- connect.php
+++ connect.php
@@ -1,13 +1,7 @@
 <?php
 include 'autoload.php';
-$servername = env('DB_HOST');
-$username = env('DB_USERNAME');
-$password = env('DB_PASSWORD');
-$dbname = env('DB_NAME');
-
-
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli(env('DB_HOST'), env('DB_USERNAME'), env('DB_PASSWORD'), env('DB_NAME'));
 
 // Check connection
 if ($conn->connect_error) {
@@ -15,14 +9,16 @@ if ($conn->connect_error) {
 }
 echo "Connected successfully<br>";
 
-$sql = "SELECT RestoName, FoodName, Price FROM AllMenu";
+$sql = "SELECT * FROM AllMenu";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
+    echo '<table class="menuTable"><tr><th>Restaurant Name</th><th>Food Item</th><th>Price</th><th>Calories</th><th>Description</th></tr>';
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo "Restaurant Name: " . $row["RestoName"]. " - Food: " . $row["FoodName"]. " " . $row["Price"]. "<br>";
+        echo "<tr><td>".$row["RestoName"]."</td><td>".$row["FoodName"]."</td><td>".$row["Price"]."</td><td>".$row["Calories"]."</td><td>".$row["Description"]."</td></tr>";
     }
+    echo "</table>";
 } else {
     echo "0 results";
 }
