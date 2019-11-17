12c2c44881a350b2b9893dce7ec449f30fbdb594
EthanBautista <raffybautista07@gmail.com>
2019-11-17 17:43:07 -0500

    added main.js to js folder

--- menu.php
+++ menu.php
@@ -7,7 +7,7 @@
     <title>ONFoodU</title>
     <link rel="stylesheet" type="text/css" href="main.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <script src="main.js"></script>
+    <script src="/js/main.js"></script>
 </head>
 
 <body>
@@ -22,9 +22,25 @@
         </div>
     </header>
     <section>
-            <?php 
-            include 'connect.php';
-            ?>
+        <div class="row">
+            <div class="column25">
+                <div class="FilterContainer">
+                    <h2>Filter</h2>
+                    <a href="./menus/menu.php">All</a>
+                    <a href="./menus/uaMenu.html">UA</a>
+                    <a href="./menus/ubMenu.html">UB</a>
+                    <a href="./menus/dcCafMenu.html">UA</a>
+                    <a href="./menus/uaMenu.html">UA</a>
+                </div>
+            </div>
+
+            <div class="column75">
+                <?php 
+                    include 'connect.php';
+                ?>
+        </div>
+        </div>
     </section>
 </body>
+
 </html>
