50706e746dc3cac599b3a4fd763dc02057d3cdbc
EthanBautista <raffybautista07@gmail.com>
2019-11-16 20:54:07 -0500

    table css

--- connect.php
+++ connect.php
@@ -7,7 +7,6 @@ $conn = new mysqli(env('DB_HOST'), env('DB_USERNAME'), env('DB_PASSWORD'), env('
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully<br>";
 
 $sql = "SELECT * FROM AllMenu";
 $result = $conn->query($sql);
@@ -16,7 +15,7 @@ if ($result->num_rows > 0) {
     echo '<table class="menuTable"><tr><th>Restaurant Name</th><th>Food Item</th><th>Price</th><th>Calories</th><th>Description</th></tr>';
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo "<tr><td>".$row["RestoName"]."</td><td>".$row["FoodName"]."</td><td>".$row["Price"]."</td><td>".$row["Calories"]."</td><td>".$row["Description"]."</td></tr>";
+        echo "<tr><td>".$row["RestoName"]."</td><td>".$row["FoodName"]."</td><td>$".$row["Price"]."</td><td>".$row["Calories"]." kcal</td><td>".$row["Description"]."</td></tr>";
     }
     echo "</table>";
 } else {
