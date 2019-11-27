875f1547965ef3a2f3dd4bbf4ac79ac31156ae98
Alexandre <alexandre@dafoe.me>
2019-11-27 07:32:00 -0500

    Added support to load specific menu location from homepage

--- menu.php
+++ menu.php
@@ -1,5 +1,5 @@
 <?php
-    session_start();
+session_start();
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -12,73 +12,73 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="./js/main.js"></script>
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"
-        integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+            integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
     <script src="./js/menuLoader.js"></script>
 </head>
 
 <body>
-    <header>
-        <div class="topnav" id="myTopnav">
-            <a href="index.php" class="active">ONFoodU</a>
-            <a href="menu.php">Menu</a>
-            <?php if (isset($_SESSION["id"])) { 
-                echo "<a href='login.php' id='loginnav'>My Account</a>";
-            } else { 
-                echo "<a href='login.php' id='loginnav'>Login</a>";
-             } ?>
-            <a href="javascript:void(0);" class="icon" onclick="navigation()">
-                <i class="fa fa-bars"></i>
-            </a>
-        </div>
-    </header>
-    <section>
-        <div class="row">
-            <div class="column25">
-                <div class="FilterContainer">
-                    Filter
-                    <a onclick="loadMenu('./api.php?QueryNum=0')">All</a>
-                    <a onclick="loadMenu('./api.php?QueryNum=2&Location=UA')">UA</a>
-                    <a onclick="loadMenu('./api.php?QueryNum=2&Location=UB%20Caf')">UB</a>
-                    <a onclick="loadMenu('./api.php?QueryNum=2&Location=DC%20Caf')">DC Caf</a>
-                    <a onclick="loadMenu('./api.php?QueryNum=3&View=CheapFood')">Cheap Food</a>
-                    <a onclick="loadMenu('./api.php?QueryNum=3&View=Location')">Location</a>
-                    <a onclick="loadMenu('./api.php?QueryNum=3&View=LowCal')">Low Calories</a>
+<header>
+    <div class="topnav" id="myTopnav">
+        <a href="index.php" class="active">ONFoodU</a>
+        <a href="menu.php">Menu</a>
+        <?php if (isset($_SESSION["id"])) {
+            echo "<a href='login.php' id='loginnav'>My Account</a>";
+        } else {
+            echo "<a href='login.php' id='loginnav'>Login</a>";
+        } ?>
+        <a href="javascript:void(0);" class="icon" onclick="navigation()">
+            <i class="fa fa-bars"></i>
+        </a>
+    </div>
+</header>
+<section>
+    <div class="row">
+        <div class="column25">
+            <div class="FilterContainer">
+                Filter
+                <a onclick="loadMenu('./api.php?QueryNum=0')">All</a>
+                <a onclick="loadMenu('./api.php?QueryNum=2&Location=UA')">UA</a>
+                <a onclick="loadMenu('./api.php?QueryNum=2&Location=UB%20Caf')">UB</a>
+                <a onclick="loadMenu('./api.php?QueryNum=2&Location=DC%20Caf')">DC Caf</a>
+                <a onclick="loadMenu('./api.php?QueryNum=3&View=CheapFood')">Cheap Food</a>
+                <a onclick="loadMenu('./api.php?QueryNum=3&View=Location')">Location</a>
+                <a onclick="loadMenu('./api.php?QueryNum=3&View=LowCal')">Low Calories</a>
 
-                </div>
+            </div>
+        </div>
+
+        <div class="column75">
+            <div id="menuListing">
+
             </div>
 
-            <div class="column75">
-                <div id="menuListing">
-
-                </div>
-
-                <script>
+            <script>
 
 
-                    <?php
-                            if(isset($_POST['Location'])){
-                                $loadURL = "./api.php?QueryNum=2&Location=".$_POST['Location'];
-                                echo "loadMenu('".$loadURL."')";
+                <?php
+                if(isset($_POST['Location'])){
+                    $loadURL = "./api.php?QueryNum=2&Location=".$_POST['Location'];
+                    echo "loadMenu('".$loadURL."')";
 
-                            } else {
-                                echo "
+                } else {
+                    echo "
                                 function selectMenu(query) {loadMenu(query);}
                                 window.onload = function () {apiURL = \"./api.php?QueryNum=0\";loadMenu(apiURL);};
                                 ";
-                            }
-                    ?>
-                </script>
+                }
+                ?>
+            </script>
 
-            </div>
         </div>
-    </section>
-    <section class="orderContainer">
-        <?php 
-            include "config.php";
-        ?>
-        <div> Order Food </div>
+    </div>
+</section>
+<section class="orderContainer">
+    <?php
+    include "config.php";
+    ?>
+    <div> Order Food </div>
 
-    </section>
+</section>
 </body>
 
 </html>
