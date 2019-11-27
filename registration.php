990a9d4ba9f26f000d54bcc1f313daa302f6f9c7
Anuj Patel <anujmaitri@gmail.com>
2019-11-27 15:09:16 -0500

    added Email Registration

--- registration.php
+++ registration.php
@@ -6,6 +6,7 @@ session_start();
 
 $name=$_POST['user'];
 $pass=$_POST['password'];
+$email=$_POST['email'];
 $hashed=hash('sha256',$pass);
 
 $s = "select * from Accounts where Name = '$name'";
@@ -17,7 +18,7 @@ $num = mysqli_num_rows($result);
 if($num==1){
 	echo "Username already exists in the system!";
 }else{
-	$reg = "insert into Accounts(Name,Password) values ('$name','$hashed')";
+	$reg = "insert into Accounts(Name,Email,Password) values ('$name','$email','$hashed')";
 	mysqli_query($con, $reg);
 	$_SESSION["id"] = $name;
 	header("Location: login.php");
