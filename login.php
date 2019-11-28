21366ebab3f650995c8452007fec02477546743d
Alexandre <alexandre@dafoe.me>
2019-11-27 22:19:32 -0500

    Raff why you break things

--- login.php
+++ login.php
@@ -1,6 +1,5 @@
-<?php 
-    session_start();
-    include 'autoload.php';
+<?php
+session_start();
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -12,28 +11,28 @@
     <link rel="stylesheet" type="text/css" href="main.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
-    <script src="./js/main.js"></script>
+    <script src="/js/main.js"></script>
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"
-        integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+            integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
     <script src="./js/viewLoader.js"></script>
 </head>
 
 <body>
-    <header>
-        <div class="topnav" id="myTopnav">
-            <a href="index.php" >ONFoodU</a>
-            <a href="menu.php">Menu</a>
-            <?php if (isset($_SESSION["id"])) { 
-                echo "<a href='login.php'class='active' id='loginnav'>My Account</a>";
-            } else { 
-                echo "<a href='login.php'class='active' id='loginnav'>Login</a>";
-             } ?>
-            <a href="javascript:void(0);" class="icon" onclick="navigation()">
-                <i class="fa fa-bars"></i>
-            </a>
-        </div>
-    </header>
-    <section class="container">
+<header>
+    <div class="topnav" id="myTopnav">
+        <a href="index.php" >ONFoodU</a>
+        <a href="menu.php">Menu</a>
+        <?php if (isset($_SESSION["id"])) {
+            echo "<a href='login.php'class='active' id='loginnav'>My Account</a>";
+        } else {
+            echo "<a href='login.php'class='active' id='loginnav'>Login</a>";
+        } ?>
+        <a href="javascript:void(0);" class="icon" onclick="navigation()">
+            <i class="fa fa-bars"></i>
+        </a>
+    </div>
+</header>
+<section class="container">
     <?php if (!isset($_SESSION["id"])) { ?>
         <div class="login-box">
             <div class="row">
@@ -72,43 +71,37 @@
             </div>
         </div>
     <?php } else {
-        echo "<div><h3>".$_SESSION["name"]." Account Page</h3></div>"; ?>
+    $id = $_SESSION['id'];
+    include 'config.php';
+    include 'autoload.php';
+    $s = "select * from Accounts where ID = '$id'";
+    $result= mysqli_query($con, $s);
+    $num = mysqli_num_rows($result);
+    if($num==1){
+        while($row = mysqli_fetch_assoc($result)){
+            $balance= $row["Balance"];
+        }
+    }
+    echo  "<div class='accHeader'>".$_SESSION["name"]."</div>";
+    ?>
 
-        <div id="AddBalance">
-            <h3>Manage Balance</h3>
-            <?php
+    <div class="balanceContainer">
 
-            // Create connection
-            $conn = new mysqli(env('DB_HOST'), env('DB_USERNAME'), env('DB_PASSWORD'), env('DB_NAME'));
+        <div class="balanceHeader">Balance: <?php echo $balance; ?></div>
+    </div>
+    <div class="orderHeader">Orders</div>
+    <div id="menuListing">
 
-            // Check connection
-            if ($conn->connect_error) {
-                echo "<p>Connection Failed</p>";
-                die("Connection failed: " . $conn->connect_error);
-            }
-
-            $sqlQuery = "SELECT Balance FROM Accounts WHERE ID=1";
-            $result = $conn->query($sqlQuery);
-            $row = mysqli_fetch_assoc($result);
-            echo "Balance $".$row['Balance'];
-            mysqli_close($conn);
-            ?>
-
-
-        </div>
-        <h3>Orders</h3>
-        <div id="menuListing"></div>
+    </div>
+    <div>
 
         <script>
             apiURL = "./api.php?QueryNum=3&View=AccountOrders";
             loadMenu(apiURL);
         </script>
-        <a href="logout.php">Logout</a>
-    <?php }?>
-    </section>
 
-
-
+        <?php }?>
+        <a href="logout.php" class="logout">Logout</a>
+</section>
 </body>
-
 </html>
